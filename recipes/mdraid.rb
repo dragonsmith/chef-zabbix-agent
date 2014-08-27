2599294d7b010b17eecf84659f2ad8651362b47f
Kirill Kouznetsov <agon.smith@gmail.com>
2014-08-28 02:58:56 +0400

    recipe[zabbix-agent::mdraid] invoke sudo resource without any checks - we are strictly relying on cookbook[sudo] from now on.

--- recipes/mdraid.rb
+++ recipes/mdraid.rb
@@ -3,6 +3,12 @@
 # Recipe:: mdraid
 #
 
+service node['zabbix']['service'] do
+  pattern  'zabbix_agentd'
+  supports [restart: true]
+  action   :nothing
+end
+
 directory '/opt/chef/zabbix/' do
   owner 'root'
   group 'root'
@@ -18,22 +24,17 @@ cookbook_file '/opt/chef/zabbix/check_md_raid' do
 end
 
 file '/etc/zabbix/agent-conf.d/md.conf' do
-  content %Q(UserParameter=dev.md[*],/usr/bin/sudo /opt/chef/zabbix/check_md_raid -d /dev/md$1)
+  content 'UserParameter=dev.md[*],/usr/bin/sudo /opt/chef/zabbix/check_md_raid -d /dev/md$1'
   owner 'root'
   group 'root'
   mode 0644
   notifies :restart, "service[#{node['zabbix']['service']}]", :delayed
 end
 
-begin
-  sudo 'zabbix-mdadm' do
-    user 'zabbix'
-    commands ['/opt/chef/zabbix/check_md_raid -d /dev/md[0-9]']
-    nopasswd true
-  end
-rescue NameError => e
-  Chef::Log.info(e.message)
-  Chef::Log.info('LWRP for sudo is not defined. Skipping resource sudo[zabbix-mdadm] ...')
+sudo 'zabbix-mdadm' do
+  user 'zabbix'
+  commands ['/opt/chef/zabbix/check_md_raid -d /dev/md[0-9]']
+  nopasswd true
 end
 
 # vim: ts=2 sw=2 et ai
