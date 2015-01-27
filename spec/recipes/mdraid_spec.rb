243c88024bfa1d1c9309dc7d1ea87c0a1b685958
Maxim Filatov <pipopolam@gmail.com>
2015-01-27 15:22:02 +0300

    Recipes: linuxsquad is now mdraid

--- spec/recipes/mdraid_spec.rb
+++ spec/recipes/mdraid_spec.rb
@@ -1,42 +1,23 @@
 require_relative '../spec_helper'
 
 describe 'zabbix-agent::mdraid' do
-  subject { ChefSpec::SoloRunner.new.converge(described_recipe) }
-  let(:config_file) { subject.file('/etc/zabbix/agent-conf.d/md.conf') }
-  let(:zabbix_service) { subject.service('zabbix-agent') }
 
-  it { expect(zabbix_service).to do_nothing }
+  subject { ChefSpec::SoloRunner.new { |node| node.set['base']['mail_to'] = 'zabbix@evilmartians.com'}.converge(described_recipe) }
 
-  it do
-    is_expected.to create_directory('/opt/chef/zabbix/').with(
-      owner: 'root',
-      mode: 'root',
-      mode: 0755,
-      recursive: true
-    )
-  end
+  it { is_expected.to_not start_service('zabbix-agent') }
 
-  it do
-    is_expected.to create_cookbook_file('/opt/chef/zabbix/check_md_raid').with(
-      owner: 'zabbix',
-      group: 'zabbix',
-      mode: 0755,
-      source: 'check_md_raid'
-    )
-  end
+  it { is_expected.to render_file('/usr/local/bin/zabbix_mdraid.sh')
+                       .with_content('AUTHOR: <The Best Zabbix admin>')}
+  it { is_expected.to render_file('/etc/zabbix/agent-conf.d/userparameter_mdraid.conf')
+                       .with_content('UserParameter=mdraid.discovery, sudo /usr/local/bin/zabbix_mdraid.sh -D')
+  }
 
-  it do
-    is_expected.to render_file('/etc/zabbix/agent-conf.d/md.conf').with_content('UserParameter=dev.md[*],/usr/bin/sudo /opt/chef/zabbix/check_md_raid -d /dev/md$1')
-  end
+  it { is_expected.to install_sudo('zabbix_mdadm')
+                       .with(user: 'zabbix',
+                             commands: ['/usr/local/bin/zabbix_mdraid.sh -D',
+                                        '/usr/local/bin/zabbix_mdraid.sh -m/dev/md[0-9]* ?*'],
+                             nopasswd: true)
+  }
 
-  it { expect(config_file).to notify('service[zabbix-agent]').to(:restart).delayed }
-
-  it do
-    is_expected.to install_sudo('zabbix-mdadm').with(
-      user: 'zabbix',
-      commands: ['/opt/chef/zabbix/check_md_raid -d /dev/md[0-9]'],
-      nopasswd: true
-    )
-  end
-
+  it { expect(subject.cookbook_file('/etc/zabbix/agent-conf.d/userparameter_mdraid.conf')).to notify('service[zabbix-agent]').to(:restart) }
 end
