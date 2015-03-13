890ce11613c773643f9b03b75b8f7086d901bf38
Maxim Filatov <pipopolam@gmail.com>
2015-03-13 17:42:21 +0300

    IOstat: recipe

---
+++ spec/recipes/iostat_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../spec_helper'
+
+describe 'zabbix-agent::iostat' do
+  subject { ChefSpec::SoloRunner.new.converge(described_recipe) }
+  let(:user_param_template) { subject.template('/etc/zabbix/agent-conf.d/userparameter_iostat.conf') }
+  let(:zabbix_service) { subject.service('zabbix-agent') }
+
+  it { expect(zabbix_service).to do_nothing }
+  it { is_expected.to install_package 'sysstat' }
+  it { is_expected.to install_package 'gawk' }
+
+  it { is_expected.to render_file('/etc/zabbix/agent-conf.d/userparameter_iostat.conf')
+      .with_content(%r{iostat.collect,/usr/local/bin/iostat-collect.sh /tmp/iostat.out}) }
+
+  it { expect(user_param_template).to notify('service[zabbix-agent]').to(:restart).delayed }
+
+  it { is_expected.to create_directory('/usr/local/bin')}
+  it { is_expected.to render_file('/usr/local/bin/iostat-collect.sh')}
+  it { is_expected.to render_file('/usr/local/bin/iostat-parse.sh')}
+end
