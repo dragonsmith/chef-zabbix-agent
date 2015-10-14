6c88df9e871e2dfc89aeda9a551bcf9b9aa5d5ef
Kirill Kouznetsov <agon.smith@gmail.com>
2015-10-14 23:27:51 +0300

    Version bump: 1.3.11

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Kirill Kouznetsov'
 maintainer_email  'agon.smith@gmail.com'
 license           'Apache 2.0'
 description       'Installs and configures zabbix agent'
-version           '1.3.10'
+version           '1.3.11'
 
 recipe            'zabbix-agent', 'Installs and configures zabbix agent'
 
