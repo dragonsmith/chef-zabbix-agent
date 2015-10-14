1e0e0190e73111dbaf1c5f08572e2fe0edd241bd
Kirill Kouznetsov <agon.smith@gmail.com>
2015-10-14 23:17:10 +0300

    Default path to redis config file is not needed inside module.

--- libraries/redis_helpers.rb
+++ libraries/redis_helpers.rb
@@ -11,17 +11,15 @@ module Zabbix
   module Redis
     # This helper provides three functions to get redis configuration parameters.
     module Helpers
-      REDIS_CONFIG = '/etc/redis/redis.conf'
-
-      def get_redis_host(config = REDIS_CONFIG)
+      def get_redis_host(config)
         ::File.open(config, 'r').grep(/^\s*bind\s/, &:split).first[1]
       end
 
-      def get_redis_port(config = REDIS_CONFIG)
+      def get_redis_port(config)
         ::File.open(config, 'r').grep(/^\s*port\s/, &:split).first[1].to_i
       end
 
-      def get_redis_password(config = REDIS_CONFIG)
+      def get_redis_password(config)
         ::File.open(config, 'r').grep(/^\s*requirepass\s/, &:split).first[1]
       end
     end
