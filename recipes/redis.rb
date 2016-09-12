0805fb06532c63654c22942b9891ab62b94def34
Kirill Kouznetsov <agon.smith@gmail.com>
2016-09-13 01:03:36 +0300

    recipe[zabbix-agent::redis] add new UserParameter and the cron task that will generate data for it

--- recipes/redis.rb
+++ recipes/redis.rb
@@ -28,4 +28,9 @@ template "#{node['zabbix']['conf_dir']}/agent-conf.d/redis-custom-user-parameter
   )
 end
 
+cron 'write zabbix_key with unixtime to redis' do
+  minute '*/1'
+  command "/usr/bin/redis-cli -a #{get_redis_password(node['zabbix']['redis_config'])} -h #{get_redis_host(node['zabbix']['redis_config'])} -p #{get_redis_port(node['zabbix']['redis_config'])} set zabbix_key #{Time.now.to_i}"
+end
+
 # zabbix_agent_redis 'main'
